cf26b97fc2193f7681037c636078ab7270ae5ae9
yaohea <1119776748@qq.com>
2021-02-05 23:40:40 +0800

    显示、循环、监听事件

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,25 @@
-Page({})
+Page({
+  data: {
+    start: '开始',
+    count: 0,
+    user: [
+      {
+        name: '姚汕',
+        age: 21
+      },
+      {
+        name: '姚汕1',
+        age: 22
+      },
+      {
+        name: '姚汕2',
+        age: 23
+      }
+    ]
+  },
+  handleBtn() {
+    this.setData({
+      count: this.data.count + 1
+    })
+  }
+})
