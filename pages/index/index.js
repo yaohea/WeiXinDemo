cead68ecbf8264e5658e8f067058c71ab9e1a8e7
yaohea <1119776748@qq.com>
2021-02-11 21:53:11 +0800

    联系

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,30 @@
-Page({})
+import request from '../../server/network.js'
+Page({
+  data: {
+    title: [{id:0,name:'流行'},{id:1,name:'精选'},{id:2,name:'时尚'}]
+  },
+  handle(e) {
+    console.log(e.detail);
+  },
+  onLoad: function (options) {
+    request({
+      url: 'http://127.0.0.1:3000/web/api/register',
+      method: 'POST',
+      data: {username: 'yaodad', password: '123456'}
+    }).then(res => console.log(res)).catch(res => console.log(res))
+    // wx.request({
+    //   url: 'http://127.0.0.1:3000/web/api/register',
+    //   method: 'POST',
+    //   data:{
+    //     username: 'qweasd',
+    //     password: '123456'
+    //   },
+    //   success: function (res) {
+    //     console.log(res);
+    //   },
+    //   fail: function (res) {
+    //     console.log(res);
+    //   }
+    // })
+  }
+})
