fa4c8baec9bd84ff0af8b6ef59a74714618d446a
yaohea <1119776748@qq.com>
2021-02-12 17:48:36 +0800

    随便写

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,16 @@
-Page({})
+Page({
+  data: {
+    list: {
+      name: 'yaoge',
+      hobby: ['西瓜','苹果','香蕉']
+    }
+  },
+  handle() {
+    let data = this.data.list.hobby
+    data.push('牛肉')
+    let str = 'list.hobby'
+    this.setData({
+      [str]: data
+    })
+  }
+})
