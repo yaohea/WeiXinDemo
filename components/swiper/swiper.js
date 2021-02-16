ed636d80ea8cb3cab950b93c4f64d1309716bc22
yaohea <1119776748@qq.com>
2021-02-16 23:53:16 +0800

    练习

---
+++ components/swiper/swiper.js
@@ -0,0 +1,29 @@
+// components/siwper/siwper.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    list: {
+      type: Array,
+      value: []
+    },
+    baseURL: {
+      type: String
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,17 +1,22 @@
 import request from '../../server/network.js'
 Page({
   data: {
-    title: [{id:0,name:'流行'},{id:1,name:'精选'},{id:2,name:'时尚'}]
+    title: [{ id: 0, name: '流行', list: ['第一', '第一', '第一'] }, { id: 1, name: '精选', list: ['第二', '第二', '第二'] }, { id: 2, name: '时尚', list: ['第三', '第三', '第三'] }],
+    active: 0,
+    activeData: 0
   },
   handle(e) {
-    console.log(e.detail);
+    this.setData({
+      active: e.target.dataset.data.id,
+      activeData: e.target.dataset.data.id
+    })
   },
   onLoad: function (options) {
-    request({
-      url: 'http://127.0.0.1:3000/web/api/register',
-      method: 'POST',
-      data: {username: 'yaodad', password: '123456'}
-    }).then(res => console.log(res)).catch(res => console.log(res))
+    // request({
+    //   url: 'http://127.0.0.1:3000/web/api/register',
+    //   method: 'POST',
+    //   data: {username: 'yaodad', password: '123456'}
+    // }).then(res => console.log(res)).catch(res => console.log(res))
     // wx.request({
     //   url: 'http://127.0.0.1:3000/web/api/register',
     //   method: 'POST',
